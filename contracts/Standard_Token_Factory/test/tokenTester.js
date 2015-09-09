24a8f8530d60075fd93df160ad4007517638196a
ethers <ethereum@outlook.com>
2015-09-08 23:30:29 -0700

    Verify that a contract that uses tokenfactory, gets the initial tokens
    a testing contract, TokenTester.sol, is required since only a contract can
    obtain the return value of the factory's createToken.
    
    Commit includes:
    - split up contracts into separate files (allows test to access each contract)
    - removed dev/test contracts.json since they are always recreated by Truffle

---
+++ contracts/Standard_Token_Factory/test/tokenTester.js
@@ -0,0 +1,13 @@
+contract("TokenTester", function(accounts) {
+    it("creates 10000 initial tokens", function(done) {
+        var tester = TokenTester.at(TokenTester.deployed_address);
+        tester.tokenContractAddress.call()
+        .then(function(tokenContractAddr) {
+            var tokenContract = Standard_Token.at(tokenContractAddr);
+            return tokenContract.coinBalanceOf.call(TokenTester.deployed_address);
+        }).then(function (result) {
+            assert.strictEqual(result.c[0], 10000);  // 10000 as specified in TokenTester.sol
+            done();
+        }).catch(done);
+    });
+});
