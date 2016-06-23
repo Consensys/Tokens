8e4237fe3c8f7627ac498addca0448eebc9f6840
Simon de la Rouviere <simon@delarouviere.com>
2016-06-23 13:24:04 +0200

    Removed some comments.

--- Token_Contracts/contracts/HumanStandardTokenFactory.sol
+++ Token_Contracts/contracts/HumanStandardTokenFactory.sol
@@ -54,12 +54,9 @@ contract HumanStandardTokenFactory {
 
     function createHumanStandardToken(uint256 _initialAmount, string _name, uint8 _decimals, string _symbol) returns (address) {
 
-        //address newTokenAddr = address(new HumanStandardToken(_initialAmount, _name, _decimals, _symbol));
-        //HumanStandardToken newToken = HumanStandardToken(newTokenAddr);
         HumanStandardToken newToken = (new HumanStandardToken(_initialAmount, _name, _decimals, _symbol));
         newToken.transfer(msg.sender, _initialAmount); //the factory will own the created tokens. You must transfer them.
         created[msg.sender].push(address(newToken));
         return address(newToken);
-        //return newTokenAddr;
     }
 }
