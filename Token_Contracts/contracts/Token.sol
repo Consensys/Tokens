db9f1844e695692f7e2e31e082f290db994a5527
ethers <ethereum@outlook.com>
2016-07-04 14:40:16 -0700

    Add comment about totalSupply

--- Token_Contracts/contracts/Token.sol
+++ Token_Contracts/contracts/Token.sol
@@ -1,4 +1,13 @@
 contract Token {
+    /* This is a slight change to the ERC20 base standard.
+    function totalSupply() constant returns (uint256 supply);
+    is replaced with:
+    uint256 public totalSupply;
+    This automatically creates a getter function for the totalSupply.
+    This is moved to the base contract since public getter functions are not
+    currently recognised as an implementation of the matching abstract
+    function by the compiler.
+    */
     /// total amount of tokens
     uint256 public totalSupply;
 
