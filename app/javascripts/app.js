2f5cf91562b50e1e6e55e3af66b86c881e7a1830
human <dip@dip-office.net>
2016-08-20 20:46:48 +0200

    [%] implement some issues from code review by sebs.
    Other code review comments were impoted as ToDos and to be cleared.

--- app/javascripts/app.js
+++ app/javascripts/app.js
@@ -1,61 +1,63 @@
-var TX_BROWSER="https://morden.ether.camp/transaction";
+"use strict"
+const TX_BROWSER="https://morden.ether.camp/transaction";
 var account_me, account_other;
 var name_me, name_other;
 var balance_me, allowed_me;
 
+//ToDo:  docs and own file
 function setErrMsg(message) {
   $("#errmsg").text(message).stop().css({opacity:100}).delay(3000).fadeTo('slow',0);
 }
 
 const MAX_LOG_ROWS=3;
 var nextRowId=0;
+//ToDo: apidcos
 function log(msgType,message,txId, rowId) {
-    var rowByTxId = $('tr[txId="'+txId+'"]');
-    var rowByRowId = $('tr#row_'+rowId);
-    var noTx = typeof txId === 'undefined';
-    if (typeof rowId === 'undefined') {
-        if (rowByTxId.length == 1) {
-            return; // Tx is already confirmed.
-        }
-        // event from unconfirmed Tx just send.
-        rowId = nextRowId++ % (MAX_LOG_ROWS+1);                         //create new rowId for new row
+  var rowByTxId = $('tr[txId="'+txId+'"]');
+  var rowByRowId = $('tr#row_'+rowId);
+  var noTx = typeof txId === 'undefined';
+  if (typeof rowId === 'undefined') {
+    if (rowByTxId.length == 1) {
+      return; // Tx is already confirmed.
     }
-    var dateTime = new Date($.now()).toLocaleTimeString().toLowerCase();
-    var newRowHtml = noTx
-        ? '<tr id="row_'+rowId+'">' +
-            '<td>'+msgType+'</td>' +
-            '<td class="message">'+message+'</td><td>'+dateTime+'</td>' +
-          '</tr>'
-        : '<tr id="row_'+rowId+'" txId="'+txId+'">' +
-            '<td><a href="'+TX_BROWSER+'/'+txId+'" target="__blank">'+msgType+'</a></td>' +
-            '<td class="message">'+message+'</td><td>'+dateTime+'</td>' +
-          '</tr>'
-        ;
-    var targetRow = rowByRowId.add(rowByTxId);
-    if (targetRow.length==0) {
-        $(newRowHtml).prependTo('table#log > tbody');
-        $('[id^="row_"]:nth-child('+MAX_LOG_ROWS+')').nextAll().remove();
-    } else {
-        targetRow.replaceWith(newRowHtml);
-    }
-    return rowId;
+    // event from unconfirmed Tx just send.
+    rowId = nextRowId++ % (MAX_LOG_ROWS+1);  //create new rowId for new row
+  }
+  var dateTime = new Date($.now()).toLocaleTimeString().toLowerCase();
+  //ToDo: not so sexy, html
+  var newRowHtml = noTx
+    ? '<tr id="row_'+rowId+'">' +
+        '<td>'+msgType+'</td>' +
+        '<td class="message">'+message+'</td><td>'+dateTime+'</td>' +
+      '</tr>'
+    : '<tr id="row_'+rowId+'" txId="'+txId+'">' +
+        '<td><a href="'+TX_BROWSER+'/'+txId+'" target="__blank">'+msgType+'</a></td>' +
+        '<td class="message">'+message+'</td><td>'+dateTime+'</td>' +
+      '</tr>'
+    ;
+  var targetRow = rowByRowId.add(rowByTxId);
+  if (targetRow.length==0) {
+    $(newRowHtml).prependTo('table#log > tbody');
+    $('[id^="row_"]:nth-child('+MAX_LOG_ROWS+')').nextAll().remove();
+  } else {
+    targetRow.replaceWith(newRowHtml);
+  }
+  //ToDo: 3 ifs in one piece of code, you might want to split this along the lines
+  return rowId;
 }
 
 function readAttributesPromise(contract, attributes){
   var allPromises=[];
+  //ToDo:  map?
   attributes.forEach(e=>{
-    try{
-      var val = contract[e]();
-  allPromises.push(val);
-  }catch(e){
-      console.log(e);
-    }
+    var val = contract[e]();
+    allPromises.push(val);
   });
   return Promise.all(allPromises).then(values =>{
-          var result={'id':contract.address};
+    var result = {'id' : contract.address};    //always add 'id' to result set
     attributes.forEach((attr,i)=>{
       result[attr]=values[i];
-  });
+    });
     return result;
   })
 }
@@ -67,37 +69,39 @@ function setupEventHandlers(){
       setErrMsg(error);
     } else {
       if (event.args._owner == account_me) {
-          log("RCVD",event.args._value+" tokens  approved for "+name_other,event.transactionHash);
-          $('#allowed').text(balance_me = event.args._value).hide().fadeIn();
+        log("RCVD",event.args._value+" tokens  approved for "+name_other,event.transactionHash);
+        $('#allowed').text(balance_me = event.args._value).hide().fadeIn();
       } else if (event.args._spender == account_me) {
-          log("RCVD",event.args._value+" tokens approved for me",event.transactionHash);
-          $('#credit').text(allowed_me = event.args._value).hide().fadeIn();
+        log("RCVD",event.args._value+" tokens approved for me",event.transactionHash);
+        $('#credit').text(allowed_me = event.args._value).hide().fadeIn();
       } else {
-          //do nothing: it is unrelated event.
+        //do nothing: it is unrelated event.
       }
       return $("#currentfund").text(balance_me.plus(allowed_me)).hide().fadeIn();
-   }
+    }
   });
   token.Transfer().watch(function (error, event) {
     if (error) {
       setErrMsg(error);
     } else {
+      //handle only events sent by me or sent to me.
       if (event.args._to==account_me || event.args._from==account_me){
         token.balanceOf(account_me, {from: account_me}).then(function (value) {
-            log("RCVD",event.args._value+" tokens received by me ",event.transactionHash);
-            $("[name=balance]").text(balance_me=value).hide().fadeIn();
-            $("#currentfund").text(balance_me.plus(allowed_me)).hide().fadeIn();
-            return token.allowance(account_me, account_other, {from: account_me});
+          log("RCVD",event.args._value+" tokens received by me ",event.transactionHash);
+          $("[name=balance]").text(balance_me=value).hide().fadeIn();
+          $("#currentfund").text(balance_me.plus(allowed_me)).hide().fadeIn();
+          return token.allowance(account_me, account_other, {from: account_me});
         }).then(function (value) {
           return $("#allowed").text(value).hide().fadeIn();
         })
       } else {
-          //do nothing: it is unrelated event.
+        //do nothing: it is an event unrelated to me.
       }
     }
   });
 }
 
+//ToDo: here you can build something testable
 function fetchTokenData() {
   var token = HumanStandardToken.deployed();
   return Promise.all([
@@ -119,48 +123,48 @@ function fetchTokenData() {
 }
 
 function transfer() {
-    var token = HumanStandardToken.deployed();
-    var amount = parseInt($("#amount_send").val());
-    if (isNaN(amount)) {
-        setErrMsg("invalid or missed amount!");
-    } else if (amount > balance_me) {
-        setErrMsg("not enough money");
-    } else {
-        var rowId;
-        token.transfer(account_other, amount, {from: account_me}).then(function (tx, err) {
-            if (err) {
-                var txId = web3.eth.getTransactionReceipt(tx).transactionHash;
-                log("ERR", amount + " tokens sent to " + name_other, txId, rowId);
-            } else {
-                var txId = web3.eth.getTransactionReceipt(tx).transactionHash;
-                log("ACK", amount + " tokens sent to " + name_other, txId, rowId);
-            }
-            return fetchTokenData();
-        });
-        rowId = log("SENT", amount + " tokens sent to " + name_other);
-    }
+  var token = HumanStandardToken.deployed();
+  var amount = parseInt($("#amount_send").val());
+  if (isNaN(amount)) {
+    setErrMsg("invalid or missed amount!");
+  } else if (amount > balance_me) {
+    setErrMsg("not enough money");
+  } else {
+    var rowId;
+    token.transfer(account_other, amount, {from: account_me}).then(function (tx, err) {
+      if (err) {
+        var txId = web3.eth.getTransactionReceipt(tx).transactionHash;
+        log("ERR", amount + " tokens sent to " + name_other, txId, rowId);
+      } else {
+        var txId = web3.eth.getTransactionReceipt(tx).transactionHash;
+        log("ACK", amount + " tokens sent to " + name_other, txId, rowId);
+      }
+      return fetchTokenData();
+    });
+    rowId = log("SENT", amount + " tokens sent to " + name_other);
+  }
 }
 
 function allow() {
   var token = HumanStandardToken.deployed();
   var amount = parseInt($("#amount_allow").val());
   if (isNaN(amount)) {
-      setErrMsg("invalid or missed amount!");
+    setErrMsg("invalid or missed amount!");
   } else if (amount > balance_me) {
-      setErrMsg("not enough money");
+    setErrMsg("not enough money");
   } else {
-      var rowId;
-      token.approve(account_other, amount, {from: account_me}).then(function(tx,err) {
-          if (err) {
-              var txId = web3.eth.getTransactionReceipt(tx).transactionHash;
-              log("ERR", amount + " tokens approved to " + name_other, txId, rowId);
-          } else {
-              var txId = web3.eth.getTransactionReceipt(tx).transactionHash;
-              log("ACK", amount + " tokens approved to " + name_other, txId, rowId);
-          }
-        fetchTokenData();
-      });
-      rowId = log("SENT", amount + " tokens approved to " + name_other);
+    var rowId;
+    token.approve(account_other, amount, {from: account_me}).then(function(tx,err) {
+      if (err) {
+        var txId = web3.eth.getTransactionReceipt(tx).transactionHash;
+        log("ERR", amount + " tokens approved to " + name_other, txId, rowId);
+      } else {
+        var txId = web3.eth.getTransactionReceipt(tx).transactionHash;
+        log("ACK", amount + " tokens approved to " + name_other, txId, rowId);
+      }
+      fetchTokenData();
+    });
+    rowId = log("SENT", amount + " tokens approved to " + name_other);
   }
 }
 
@@ -170,31 +174,31 @@ function claim() {
   if (isNaN(amount)) {
     setErrMsg("invalid or missed amount!");
   } else {
-      var rowId;
-      token.transferFrom(account_other, account_me, amount, {from: account_me}).then(function (tx, err) {
-          if (err) {
-              var txId = web3.eth.getTransactionReceipt(tx).transactionHash;
-              log("ERR", amount + " tokens transfered from " + name_other, txId, rowId);
-          } else {
-              var txId = web3.eth.getTransactionReceipt(tx).transactionHash;
-              log("ACK", amount + " tokens transfered from " + name_other, txId, rowId);
-          }
-          fetchTokenData();
-      });
-      rowId = log("SENT", amount + " tokens transfered from " + name_other);
+    var rowId;
+    token.transferFrom(account_other, account_me, amount, {from: account_me}).then(function (tx, err) {
+      if (err) {
+        var txId = web3.eth.getTransactionReceipt(tx).transactionHash;
+        log("ERR", amount + " tokens transfered from " + name_other, txId, rowId);
+      } else {
+        var txId = web3.eth.getTransactionReceipt(tx).transactionHash;
+        log("ACK", amount + " tokens transfered from " + name_other, txId, rowId);
+      }
+      fetchTokenData();
+    });
+    rowId = log("SENT", amount + " tokens transfered from " + name_other);
   }
 }
 
 function setupOnEnter(){
-    ['send','allow','claim'].forEach(actionName => {
-        $('#amount_'+actionName).keypress(function(e) {
-            if(e.which == 13) {
-                $(this).blur();
-                $('#'+actionName).focus().click().blur();
-                $(this).focus();
-            }
-        });
-    })
+  ['send','allow','claim'].forEach(actionName => {
+    $('#amount_'+actionName).keypress(function(e) {
+      if(e.which == 13) {
+        $(this).blur();
+        $('#'+actionName).focus().click().blur();
+        $(this).focus();
+      }
+    });
+  })
 }
 
 window.onload = function() {
@@ -203,12 +207,10 @@ window.onload = function() {
       alert("There was an error fetching your accounts.");
       return;
     }
-
     if (accounts.length == 0) {
       alert("Couldn't get any accounts! Make sure your Ethereum client is configured correctly.");
       return;
     }
-
     var url = window.location.href;
     if (url.substring(url.indexOf("?")+1) == 'bob'){
       name_me='Bob';
