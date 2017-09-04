ed395948b37a1e82b0190ce79885b3c0ee81e923
Maurelian <maurelian@protonmail.ch>
2017-09-04 11:42:51 -0400

    Merge latest changes from staging to master (#78)
    * Update README.md
    
    * Moved the funny quote to look a bit less out in the middle of nowhere
    * Links for convenience
    * Clarify how to contribute
    
    * Update README.md
    
    * Test async await (#71)
    
    * Rewrite tests with async/await
    
    * Rewrite tests with async/await
    
    * Move helpers to utils.js, remove todos
    
    * add blank lines between sub-tests, refactor the normal ether transfer test
    
    * Await test result

--- test/humanStandardTokenFactory.js
+++ test/humanStandardTokenFactory.js
@@ -1,20 +1,11 @@
-var HumanStandardTokenFactory = artifacts.require('./HumanStandardTokenFactory.sol')
+const HumanStandardTokenFactory = artifacts.require('HumanStandardTokenFactory')
 
 contract('HumanStandardTokenFactory', function (accounts) {
-  it('Verify a Human Standard Token once deployed using both verification functions.', function () {
-    var factory = null
-    var newTokenAddr = null
-    return HumanStandardTokenFactory.new()
-    .then(function (ctr) {
-      factory = ctr
-      return factory.createHumanStandardToken.call(100000, 'Simon Bucks', 2, 'SBX', {from: accounts[0]})
-    }).then(function (_newTokenAddr) {
-      newTokenAddr = _newTokenAddr
-      return factory.createHumanStandardToken(100000, 'Simon Bucks', 2, 'SBX', {from: accounts[0]})
-    }).then(function () {
-      return factory.verifyHumanStandardToken.call(newTokenAddr, {from: accounts[0]})
-    }).then(function (res) {
-      assert(res, 'Could not verify the token.')
-    }).catch((err) => { throw new Error(err) })
+  it('Verify a Human Standard Token once deployed using both verification functions.', async () => {
+    const factory = await HumanStandardTokenFactory.new()
+    const newTokenAddr = await factory.createHumanStandardToken.call(100000, 'Simon Bucks', 2, 'SBX', {from: accounts[0]})
+    await factory.createHumanStandardToken(100000, 'Simon Bucks', 2, 'SBX', {from: accounts[0]})
+    const res = await factory.verifyHumanStandardToken.call(newTokenAddr, {from: accounts[0]})
+    assert(res, 'Could not verify the token.')
   })
 })
