e956c67ec58f61c34c686cef2ef881552b6582cf
Mike Goldin <mike.goldin@consensys.net>
2017-09-04 10:57:57 -0400

    Await test result

--- test/humanStandardTokenFactory.js
+++ test/humanStandardTokenFactory.js
@@ -5,7 +5,7 @@ contract('HumanStandardTokenFactory', function (accounts) {
     const factory = await HumanStandardTokenFactory.new()
     const newTokenAddr = await factory.createHumanStandardToken.call(100000, 'Simon Bucks', 2, 'SBX', {from: accounts[0]})
     await factory.createHumanStandardToken(100000, 'Simon Bucks', 2, 'SBX', {from: accounts[0]})
-    const res = factory.verifyHumanStandardToken.call(newTokenAddr, {from: accounts[0]})
+    const res = await factory.verifyHumanStandardToken.call(newTokenAddr, {from: accounts[0]})
     assert(res, 'Could not verify the token.')
   })
 })
