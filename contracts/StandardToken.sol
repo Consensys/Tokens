5caa1d398bd3e541c7075343383f79d46283e562
Amir Bandeali <abandeali1@gmail.com>
2017-09-22 16:32:27 -0700

    Only modify allowance mapping if allowance < max uint

--- contracts/StandardToken.sol
+++ contracts/StandardToken.sol
@@ -12,6 +12,8 @@ import "./Token.sol";
 
 contract StandardToken is Token {
 
+    uint256 constant MAX_UINT256 = 2**256 - 1;
+
     function transfer(address _to, uint256 _value) returns (bool success) {
         //Default assumes totalSupply can't be over max (2^256 - 1).
         //If your token leaves out totalSupply and can issue more tokens as time goes on, you need to check if it doesn't wrap.
@@ -27,10 +29,13 @@ contract StandardToken is Token {
     function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {
         //same as above. Replace this line with the following if you want to protect against wrapping uints.
         //require(balances[_from] >= _value && allowed[_from][msg.sender] >= _value && balances[_to] + _value > balances[_to]);
-        require(balances[_from] >= _value && allowed[_from][msg.sender] >= _value);
+        uint256 allowance = allowed[_from][msg.sender];
+        require(balances[_from] >= _value && allowance >= _value);
         balances[_to] += _value;
         balances[_from] -= _value;
-        allowed[_from][msg.sender] -= _value;
+        if (allowance < MAX_UINT256) {
+            allowed[_from][msg.sender] -= _value;
+        }
         Transfer(_from, _to, _value);
         return true;
     }
