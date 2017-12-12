a71db208d00d3be928a18254469f6c5904a0f3ac
Gonçalo Sá <goncalo05@gmail.com>
2017-12-12 15:58:13 +0000

    Fix linting errors

--- test/StandardERC721.js
+++ test/StandardERC721.js
@@ -1,17 +1,17 @@
-const expectThrow = require('./utils').expectThrow;
-const TestERC721ImplementationAbstraction = artifacts.require('TestERC721Implementation');
-let ERC721;
+// const expectThrow = require('./utils').expectThrow
+const TestERC721ImplementationAbstraction = artifacts.require('TestERC721Implementation')
+let ERC721
 
 contract('TestERC721Implementation', function (accounts) {
   beforeEach(async () => {
-    //todo: deployment is OOG-ing.
-    ERC721 = await TestERC721ImplementationAbstraction.new({gas: 6700000, from: accounts[0]});
+    // todo: deployment is OOG-ing.
+    ERC721 = await TestERC721ImplementationAbstraction.new({gas: 6700000, from: accounts[0]})
   })
 
   it('creation: admin should be set', async () => {
-    const admin = await ERC721.admin.call();
-    assert.strictEqual(admin, accounts[0]);
-  });
+    const admin = await ERC721.admin.call()
+    assert.strictEqual(admin, accounts[0])
+  })
 
-  //todo: add other tests
+  // todo: add other tests
 })
