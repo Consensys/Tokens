203aedf3d66e16dd3a0f4a284f5b4ef1dd1cd218
Mike Goldin <mike.goldin@consensys.net>
2017-12-13 11:53:54 -0500

    Remove dead code

--- contracts/SampleRecipientThrow.sol
+++
@@ -1,8 +0,0 @@
-/*
-This is an example contract that helps test the functionality of the approveAndCall() functionality of HumanStandardToken.sol.
-This one will throw and thus needs to propagate the error up.
-*/
-pragma solidity ^0.4.8;
-
-contract SampleRecipientThrow {
-}
