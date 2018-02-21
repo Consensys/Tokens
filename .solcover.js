fa893941a84ecd3ac1fcf67975edcd52e061db89
maurelian <maurelian@protonmail.ch>
2018-02-21 16:41:38 -0500

    Working coverage config

---
+++ .solcover.js
@@ -0,0 +1,11 @@
+module.exports = {
+    compileCommand: '../node_modules/.bin/truffle compile',
+    testCommand: '../node_modules/.bin/truffle test --network coverage',
+    // port: 6545,
+    // testrpcOptions: '-p 6545 -u 0x54fd80d6ae7584d8e9a19fe1df43f04e5282cc43',
+    // testCommand: 'mocha --timeout 5000',
+    // norpc: true,
+    // dir: './secretDirectory',
+    // copyPackages: ['zeppelin-solidity'],
+    // skipFiles: ['Routers/EtherRouter.sol']
+};
