af7fdc53496f012d37bc4115ec30d6f364c2c172
maurelian <maurelian@protonmail.ch>
2018-02-28 15:21:40 -0500

    Conform to Mike

--- test/helpers/assertRevert.js
+++ test/helpers/assertRevert.js
@@ -2,10 +2,11 @@ module.exports = {
   assertRevert: async (promise) => {
     try {
       await promise;
-      assert.fail('Expected revert not received');
     } catch (error) {
       const revertFound = error.message.search('revert') >= 0;
       assert(revertFound, `Expected "revert", got ${error} instead`);
+      return;
     }
+    assert.fail('Expected revert not received');
   },
 };
