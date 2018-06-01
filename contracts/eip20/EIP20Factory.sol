49fa9e29f8de149e9a27aef14464e431f7d150c1
maurelian <maurelian@protonmail.ch>
2018-06-01 15:51:03 -0400

    Fix factory tests after rebase to staging

--- contracts/eip20/EIP20Factory.sol
+++ contracts/eip20/EIP20Factory.sol
@@ -1,4 +1,4 @@
-import "./SafeEIP20.sol";
+import "./EIP20.sol";
 
 pragma solidity ^0.4.21;
 
@@ -37,7 +37,7 @@ contract EIP20Factory {
         public
     returns (address) {
 
-        SafeEIP20 newToken = (new SafeEIP20(_initialAmount, _name, _decimals, _symbol));
+        EIP20 newToken = (new EIP20(_initialAmount, _name, _decimals, _symbol));
         created[msg.sender].push(address(newToken));
         isEIP20[address(newToken)] = true;
         //the factory will own the created tokens. You must transfer them.
