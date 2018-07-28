8feaf63111893349ae15091ffa3d4e2afa651607
Robert Valta <robert.j.valta@gmail.com>
2018-07-29 01:54:42 +0300

    Fix linting

--- truffle.js
+++ truffle.js
@@ -1,24 +1,24 @@
-const HDWalletProvider = require('truffle-hdwallet-provider');
-const fs = require('fs');
+const HDWalletProvider = require('truffle-hdwallet-provider')
+const fs = require('fs')
 
 // First read in the secrets.json to get our mnemonic
-let secrets;
-let mnemonic;
+let secrets
+let mnemonic
 if (fs.existsSync('secrets.json')) {
-  secrets = JSON.parse(fs.readFileSync('secrets.json', 'utf8'));
-  mnemonic = secrets.mnemonic;
+  secrets = JSON.parse(fs.readFileSync('secrets.json', 'utf8'))
+  mnemonic = secrets.mnemonic
 } else {
   console.log(
     'No secrets.json found. If you are trying to publish EPM ' +
-      'this will fail. Otherwise, you can ignore this message!',
-  );
-  mnemonic = '';
+      'this will fail. Otherwise, you can ignore this message!'
+  )
+  mnemonic = ''
 }
 
 module.exports = {
   networks: {
     live: {
-      network_id: 1, // Ethereum public network
+      network_id: 1 // Ethereum public network
       // optional config values
       // host - defaults to "localhost"
       // port - defaults to 8545
@@ -29,17 +29,17 @@ module.exports = {
     ropsten: {
       provider: new HDWalletProvider(mnemonic, 'https://ropsten.infura.io/'),
       network_id: '3',
-      gas: 5300000,
+      gas: 5300000
     },
     testrpc: {
-      network_id: 'default',
+      network_id: 'default'
     },
     coverage: {
       host: 'localhost',
       network_id: '*',
       port: 8555,
       gas: 0xfffffffffff,
-      gasPrice: 0x01,
-    },
-  },
-};
+      gasPrice: 0x01
+    }
+  }
+}
