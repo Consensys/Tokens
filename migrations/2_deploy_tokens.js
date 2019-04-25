7d6290feb7bc3e55ce4dcb13aaa88f0d9a4128c2
pau1m <paul@metahook.op>
2019-04-25 09:23:33 +0100

    Update deployment params

--- migrations/2_deploy_tokens.js
+++ migrations/2_deploy_tokens.js
@@ -1,5 +1,6 @@
 const EIP20 = artifacts.require('./EIP20.sol');
 
 module.exports = (deployer) => {
-  deployer.deploy(EIP20, 10000, 'Simon Bucks', 1, 'SBX');
+  // 420,000,000 eBudz
+  deployer.deploy(EIP20, '420000000000000000000000000', 'eBudz', 18, 'EBUD');
 };
